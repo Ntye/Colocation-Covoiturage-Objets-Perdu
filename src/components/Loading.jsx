3e683c1fafacdd13f80df6f4281ac29b12c1b0b1
Ntye <ntye.nina@gmail.com>
2024-06-15 15:22:54 +0100

    initial commit

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 function Loading () {
 	return (
-		<div>
+		<div className="d-flex align-middle justify-content-around">
 			<h2>Loading</h2> <br/> <Spinner animation="border" variant="secondary"/>
 		</div>
 	)
--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Navigate, createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import App from "./App.jsx"
 import Loading from './components/Loading.jsx'
